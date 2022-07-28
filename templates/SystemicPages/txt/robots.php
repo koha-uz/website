589d7c77b54504688590308be52a46947d59191c
Marat Dallin <mail@dallin.uz>
2022-07-28 10:12:39 +0500

    Fix mini.php and robots.txt
     On branch master
     Changes to be committed:
    	new file:   plugins/Panel/templates/layout/mini.php
    	modified:   templates/SystemicPages/txt/robots.php

--- templates/SystemicPages/txt/robots.php
+++ templates/SystemicPages/txt/robots.php
@@ -1,7 +1,7 @@
 User-agent: *
 Allow: /
-Disallow: /ru/login
-Disallow: /uz/login
+Disallow: /ru/founder/login
+Disallow: /uz/founder/login
 Sitemap: <?= $this->Url->build(['_name' => 'sitemap', '_ext' => 'xml'], ['fullBase' => true]) ?>
 
 Host: <?= $this->Url->build(['_name' => 'home'], ['fullBase' => true]) ?>
